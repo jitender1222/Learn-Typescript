8dcf808f0c773ac19bc7a08e466ab7aa0d9df52c
jitender singh <jeetusinghkanyal@gmail.com>
2022-12-09 20:28:24 +0530

    adding class

---
+++ class.ts
@@ -0,0 +1,34 @@
+
+// define a class in TS
+
+// class User{
+//     // email:String
+//     // name:String
+//     // city:String
+
+//     constructor(public email:String, public name:String){
+//         // this.email=email;
+//         // this.name=name
+//     }
+// }
+
+// const obj=new User("j@h.com","jitender singh"); //defining the object
+
+
+// we can also make the class as public and private 
+
+
+// we can write the abovve class also in a different way like the below one
+
+
+
+// class student{
+
+//     constructor(public email:String, 
+//         public name:String
+//         ){}
+// }
+
+// const obj1=new student("jitender@gmail.com","jitender")
+
+export{}
