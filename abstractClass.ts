a8c8c0faad2054c3e432b59d3a57446a26ca9317
jitender singh <jeetusinghkanyal@gmail.com>
2022-12-10 18:13:09 +0530

    adding abstract class

---
+++ abstractClass.ts
@@ -0,0 +1,16 @@
+
+
+//  abstract classes are similar to interface but after making thse classes we do not make the objects for this classes
+
+
+abstract class TakePhoto{
+
+    constructor(public cameraMode:String , public filter:String){}
+}
+
+// const obj=new TakePhoto("email","email")  error we cannot make the object of instance class
+
+
+// abstract class not complete 
+
+// rest of the videos not cover 
